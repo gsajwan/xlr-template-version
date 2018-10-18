57ac6d196bfa0ef388f4504d2d83c82b54256c91
gaurav sajwan <gsajwan@xebialabs.com>
2018-10-18 10:41:06 -0400

    gaurav

---
+++ XLRelease/ORT/test.groovy
@@ -0,0 +1,16 @@
+// Exported from:        http://Gauravs-MacBook-Pro.local:5516/#/templates/Folderda658fedcd344ebeb88f406750ccbf28-Release8b4e3904c9f74c23a5cf6b785902e502/releasefile
+// XL Release version:   8.2.0
+// Date created:         Thu Oct 18 10:41:05 EDT 2018
+
+xlr {
+  template('test') {
+    folder('ORT')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-10-18T09:00:00-0400')
+    phases {
+      phase('New Phase') {
+        
+      }
+    }
+    
+  }
+}
